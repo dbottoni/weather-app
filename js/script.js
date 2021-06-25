f5a280672f0f46d5553ecb615a92202a3cd858c1
Dana Bottoni <danabottoni@gmail.com>
2021-06-25 19:35:50 -0400

    button testing

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-// function searchFunction(){
-//   var searchTerm = document.querySelector('#searchTerm').value;
-//   fetch(
-//     'api.openweathermap.org' +
-//     searchTerm + 
-//     'fabb0e1d664af0eebed6cbacc7200253'
-//   )
-//     .then(function(response){
-//       return response.json();
-//     })
-//     .then(function (response) {  
-//     console.log(response.data[0]);
+function austinWeather(){
+  var austin = document.getElementById('#btnAustin').value;
+  fetch(
+    'https://api.openweathermap.org/data/2.5/onecall?lat=30.2672&lon=-97.7431&appid=fabb0e1d664af0eebed6cbacc7200253' + 
+    austin + 
+    '&api_key=fabb0e1d664af0eebed6cbacc7200253'
+  )
+    .then(function (response) { 
+      return response.json();
+    })
+    .then(function (response) {  
+  
+      // here down, need to re-work this to get the city name to append to html line 39
+      var responseContainerEl = document.querySelector('#city');
 
-//     })
+      responseContainerEl.innerHTML = '';
 
-
-// }
-
-
+      var city = document.createElement('h2');
+      city.setAttribute('Austin', 'value');
+      responseContainerEl.appendChild(city);
+  });
+}
