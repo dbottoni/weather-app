31e32ffa3e1a09e77d404115e08a03da1bc46a5f
Dana Bottoni <danabottoni@gmail.com>
2021-06-25 17:36:05 -0400

    html and bootstrap additions

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// function searchFunction(){
+//   var searchTerm = document.querySelector('#searchTerm').value;
+//   fetch(
+//     'api.openweathermap.org' +
+//     searchTerm + 
+//     'fabb0e1d664af0eebed6cbacc7200253'
+//   )
+//     .then(function(response){
+//       return response.json();
+//     })
+//     .then(function (response) {  
+//     console.log(response.data[0]);
+
+//     })
+
+
+// }
+
+
