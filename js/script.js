9c5cea04d10a0f96ff2fc106d348cbd8e29d05b1
Dana Bottoni <danabottoni@gmail.com>
2021-06-29 17:46:38 -0400

    second api fetch

--- js/script.js
+++ js/script.js
@@ -14,35 +14,51 @@ searchButton.addEventListener('click', function(){
   .then(response=> response.json())
   .then(data => {
     var nameValue = data['name'];
-    //var tempValue = data['main']['temp'] + '&deg';
-    //var fahrenheit = Math.round(((parseFloat(main.temp)-273.15)*1.8)+32);
+    console.log(data);
+
     var tempValue = Math.round(((parseFloat(data['main']['temp'])-273.15)*1.8)+32) + '&deg';
     var windValue = data['wind']['speed'] + 'MPH';
     var humidValue = data['main']['humidity'];
-    //var uvValue = data['weather']['id'];
+  
 
     city.innerHTML = nameValue;
     temp.innerHTML = tempValue;
     wind.innerHTML = windValue;
-    console.log(windValue);
     humidity.innerHTML = humidValue;
-    // uvindex.innerHTML = uvValue;
-    // console.log(uvValue)
+  
+
+    fetch (`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=fabb0e1d664af0eebed6cbacc7200253`)
+    .then(response=> response.json())
+    .then(data => {
+      
+      var uvValue = data.current.uvi;
+      
+      uvindex.innerHTML= uvValue;
+      console.log(uvValue);
+
+    
+      console.log(data);
+
+
+
+
     })
 });
+});
+
+
 
 
 // using workday scheduler js to guide the build for the weather app
 // also pulling info from module 6 challenge 
-var days = [1,2,3,4,5];
+// var days = [1,2,3,4,5];
   
-for (var i = 0; i < days.length; i++){
+// for (var i = 0; i < days.length; i++){
   
-  var dayEl = document.createElement("p");
-  dayEl.classList = "list-item flex-row justify-space-between align-center";
+//   var dayEl = document.createElement("p");
+//   dayEl.classList = "list-item flex-row justify-space-between align-center";
 
-  var weatherEl = document.createElement("span");
-  weatherEl.textContent = tempValue;
+//   var weatherEl = document.createElement("span");
+//   weatherEl.textContent = tempValue;
 
-  dayEl.appendChild(weatherEl);
-};
+//   dayEl.appendChild(weatherEl);
