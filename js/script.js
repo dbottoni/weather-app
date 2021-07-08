7115cef50530c5202790094e10a33c8700c348f3
Dana Bottoni <danabottoni@gmail.com>
2021-07-08 17:21:25 -0400

    added functionality to uvVale

--- js/script.js
+++ js/script.js
@@ -7,8 +7,6 @@ var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity');
 var uvindex = document.querySelector('.uvindex');
 var fiveDayContainer = document.querySelector('.searchList');
-var savedButton = document.querySelector('.saved-button');
-
 
 
 function fetchWeatherData(cityName){
@@ -17,15 +15,11 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q='+ cityName +
   .then(response=> response.json())
   .then(data => {
     var nameValue = data['name'];
-    console.log(data);
     var tempValue = Math.round(((parseFloat(data['main']['temp'])-273.15)*1.8)+32) + '&deg';
     var windValue = data['wind']['speed'] + 'MPH';
     var humidValue = data['main']['humidity'];
     var currentHour = moment.unix(data.dt).format('MMMM Do YYYY');
-    console.log(currentHour);
-    console.log(data['dt']);
 
-    
     city.innerHTML = nameValue;
     date.innerHTML = currentHour;
     temp.innerHTML = "Temp: " + tempValue + 'F';
@@ -38,8 +32,17 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q='+ cityName +
     .then(data => {
       
       var uvValue = data.current.uvi;
-      console.log(data); 
       uvindex.innerHTML= "UV Index: " + uvValue;
+
+      if (uvValue > 0 && uvValue <= 3.5){
+        $(`#uv-color`).addClass("low");
+      } else if (uvValue > 3.5 &&  uvValue <= 6.5){
+        $(`#uv-color`).addClass("moderate");
+      } else if (uvValue > 6.5 &&  uvValue <= 10){
+        $(`#uv-color`).addClass("high");
+      }
+
+    
       
         var dateFive = [1,2,3,4,5];
         for (var i = 0; i < dateFive.length; i++){
@@ -47,25 +50,20 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q='+ cityName +
         var cardDate = moment.unix(data.daily[i].dt).format('MM/DD/YY');
         var dateEl = document.getElementById(`card-date-${dateFive[i]}`);
         dateEl.innerHTML = cardDate;
-
-
+      
         }
+
       // Insert Data in Cards
       var cardIndex = [1,2,3,4,5]; 
 
       for (var i = 0; i < cardIndex.length; i++){
-        
-        // var cardDate = moment.unix(data.daily[i].dt).format('MM/DD/YY');
-        // var dateEl = document.getElementById(`card-date-${cardIndex[i]}`);
-        // dateEl.innerHTML = cardDate;
 
         var tempDay = Math.round(((parseFloat(data['hourly'][i]['temp'])-273.15)*1.8)+32) + '&deg' + 'F';        
         var tempEl = document.getElementById(`card-temp-${cardIndex[i]}`);
         tempEl.innerHTML = `Temp: ${tempDay}`;   
         
         var iconcode = data.daily[0].weather[0].icon;
-        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";      
         $(`#wicon-${cardIndex[i]}`).attr('src', iconurl);
 
         var windDay = data['daily'][i]['wind_speed'] + 'MPH';
@@ -81,24 +79,17 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q='+ cityName +
     })
 });
 
-
 }
-
 
 
 searchButton.addEventListener('click', function(){
   fetchWeatherData(inputValue.value);
-
-  var btn = document.createElement("BUTTON");
+ 
+  var btn = document.createElement("button");
   btn.innerHTML = inputValue.value;
-  console.log("search", document.querySelector('.searchList'));
-  document.querySelector('.searchList').appendChild(btn);
   
-
-
+  document.querySelector('.searchList').appendChild(btn);
+  btn.setAttribute('style', 'background-color: #0069d9');
 
 
 });
-
-
- 
